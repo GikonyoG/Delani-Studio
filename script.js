8f9d8ebea9dbc8c5def21a49b88fd763860bada1
GG <gikonyogitau1@gmail.com>
2022-03-18 00:08:17 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const name = document.getElementById("name")
-const email = document.getElementById("email")
-const form = document.getElementById("form")
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const Submittedprompt = document.getElementById("submitted");
 
+// form.addEventListener("submit", (e) => {
+//   let message = [];
 
+//   if (name.value.length > 1 && email.value.length > 1) {
+//     message.push("Form Submitted");
+//   }
+//   if (message.length > 0) {
+//     e.preventDefault();
+
+//     Submittedprompt.innerText = message.join(", ");
+//   }
+// });
 form.addEventListener("submit", (e) => {
-    let message =[]
+    e.preventDefault()
 
-    if(name.value.length > 1 && email.value.length > 1){
-        message.push("Form Submitted")
+}) 
 
-        
-    }
-  e.preventDefault();
-  getvalues();
-});
+
