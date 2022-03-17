9d343635ae09b199b934d9df9f9b9e17f86e0bb5
GG <gikonyogitau1@gmail.com>
2022-03-18 00:21:08 +0300

    initial commit

--- script.js
+++ script.js
@@ -3,21 +3,17 @@ const email = document.getElementById("email");
 const form = document.getElementById("form");
 const Submittedprompt = document.getElementById("submitted");
 
-// form.addEventListener("submit", (e) => {
-//   let message = [];
-
-//   if (name.value.length > 1 && email.value.length > 1) {
-//     message.push("Form Submitted");
-//   }
-//   if (message.length > 0) {
-//     e.preventDefault();
-
-//     Submittedprompt.innerText = message.join(", ");
-//   }
-// });
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  let message = [];
 
-}) 
+  if (name.value.length > 1 && email.value.length > 1) {
+    message.push("Form Submitted");
+  }
+  if (message.length > 0) {
+    e.preventDefault();
+
+    Submittedprompt.innerText = message.join(", ");
+  }
+});
 
 
