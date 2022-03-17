70f1713c11008a31712fbc9e53f9d9fb21dc610c
GG <gikonyogitau1@gmail.com>
2022-03-17 15:58:59 +0300

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const name = document.getElementById("name")
 const email = document.getElementById("email")
 const form = document.getElementById("form")
+
+
+form.addEventListener("submit", (e) => {
+    let message =[]
+
+    if(name.value.length > 1 && email.value.length > 1){
+        message.push("Form Submitted")
+
+        
+    }
+  e.preventDefault();
+  getvalues();
+});
